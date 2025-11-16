e9f57766aed037b632ff4cad020ab693030f7a21
Hazim Bakar <hazimbaa@gmail.com>
2025-11-17 00:34:37 +0800

    tailwind install

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="text-2xl text-red-500 font-bold">
+        Edit app/index.tsx to edit this screen. 1
+      </Text>
+    </View>
+  );
+}
